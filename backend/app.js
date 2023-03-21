308d9b6adace740d6d90cbb8cd8243f03aedd159
Jenny Svensson <jenny.svensson@medieinstitutet.se>
2023-03-21 08:52:07 +0100

    apply endpoints for POST, GET all and GET id users

--- backend/app.js
+++ backend/app.js
@@ -5,8 +5,24 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var ordersRouter = require('./routes/orders');
+var productsRouter = require('./routes/products');
+
+const mongoose = require('mongoose');
 
 var app = express();
+
+async function init () {
+    try {
+        const options = {useNewUrlParser: true, useUnifiedTopology: true};
+        await mongoose.connect('mongodb://127.0.0.1:27017/jenny-svensson', options);
+        console.log("connected to database!");
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+init();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -15,6 +31,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/orders', ordersRouter);
+app.use('/api/products', productsRouter);
 
 module.exports = app;
