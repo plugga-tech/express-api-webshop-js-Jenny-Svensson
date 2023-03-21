6428d989dfac4ed9aa6e2d02477d371c1a63352e
Jenny Svensson <jenny.svensson@medieinstitutet.se>
2023-03-21 09:04:46 +0100

    create GET all, GET id and POST new product

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,9 +1,45 @@
 var express = require('express');
 var router = express.Router();
+const Model = require('../models/products');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Products');
+/* GET all products */
+router.get('/', async (req, res, next) => {
+  try {
+    const data = await Model.find();
+    res.json(data)
+  }
+  catch(error) {
+    res.status(500).json({ message: error.message })
+  }
 });
+
+/* GET specific product id. */
+router.get('/:id', async (req, res, next) => {
+  try {
+    const data = await Model.findById(req.params.id);
+    res.json(data)
+  }
+  catch(error) {
+    res.status(500).json({ message: error.message });
+  }
+})
+
+/* POST new product. */
+router.post('/add', async (req, res) => {
+  const data = new Model({
+      name: req.body.name,
+      description: req.body.description,
+      price: req.body.price,
+      lager: req.body.lager
+  })
+
+  try {
+      const dataToSave = await data.save();
+      res.status(200).json(dataToSave)
+  }
+  catch (error) {
+      res.status(400).json({message: error.message})
+  }
+})
 
 module.exports = router;
